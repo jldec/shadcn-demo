e7cafc7140d19402a08d387dad1fdcfa84a1f9f9
jldec <jurgen@jldec.me>
2025-04-19 20:19:43 -0400

    init minimal react + vite + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './index.css'
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl">Hello World</h1>
+    </div>
+  )
+}
